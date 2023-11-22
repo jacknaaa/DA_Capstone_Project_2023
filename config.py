56efe952e093f52a45a45eef524c3d477ebf1bf3
unknown <c.sathanurukwong@gmail.com>
2023-11-22 06:24:11 -0500

    try to setup SQL

---
+++ config.py
@@ -0,0 +1,8 @@
+# DB config
+DB_CONFIG = {
+    'dbname': 'popular_movies',
+    'user': 'postgres',
+    'password': 'password',
+    'host': 'localhost',
+    'port': '5432'
+}
--- movie_scraper.py
+++ movie_scraper.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import re
+import pandasql as psql
+from config import DB_CONFIG
+import psycopg2
+from sqlalchemy import create_engine
 
 
 def scrape_most_popular_movies():
@@ -56,9 +60,79 @@ def scrape_most_popular_movies():
             f"Failed to retrieve the page. Status code: {response.status_code}")
         return None
 
+# data cleaning and a SQL join. We'll create a second dataset with additional movie information and then perform a join using the pandasql library.
+
+
+def join_two_df_by_SQL(df1, df2):
+
+    print(df1)
+
+
+def df_to_SQL(df):
+    # Extracting database connection details from the configuration
+    dbname = DB_CONFIG['dbname']
+    user = DB_CONFIG['user']
+    password = DB_CONFIG['password']
+    host = DB_CONFIG['host']
+    port = DB_CONFIG['port']
+
+    # Construct the connection string
+    connection_string = f"dbname={dbname} user={user} password={password} host={host} port={port}"
+    # Establish a connection to the PostgreSQL database
+    try:
+        connection = psycopg2.connect(connection_string)
+        print("Connected to the database!")
+
+        # Create a SQLAlchemy engine
+        engine = create_engine(
+            f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}")
+
+        # Insert the DataFrame into the database
+        df.to_sql('st_details', engine, if_exists='replace', index=False)
+
+        print("Data inserted into PostgreSQL!")
+
+        # Create a cursor object to execute SQL queries
+        cursor = connection.cursor()
+
+        # Example: Execute a simple SQL query
+        cursor.execute("SELECT * FROM st_details")
+        rows = cursor.fetchall()
+
+        print(rows)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Close the cursor and connection
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Connection closed.")
+
 
 # Call the function to scrape data for most popular movies
-popular_movies_dataframe = scrape_most_popular_movies()
+popular_mov_df = scrape_most_popular_movies()
 
-# Print the first 5 rows of the DataFrame
-print(popular_movies_dataframe.head())
+###########  cleaning data  ###########
+# replacing na values in college with No college
+popular_mov_df['Vote count'].fillna("", inplace=True)
+# replacing na values in college with No college
+# popular_mov_df['Vote count'].fillna(method='bfill', inplace=True)
+
+# print(popular_mov_df.info())
+# print(popular_mov_df.isnull().sum())
+# print(popular_mov_df['Vote count'])
+###########  cleaning data  ###########
+
+########## DataFram to SQL ##########
+df_to_SQL(popular_mov_df)
+########## DataFram to SQL ##########
+
+# Create a second dataset with additional movie information
+additional_movie_data = pd.DataFrame({
+    'Title': ['The Shawshank Redemption', 'The Godfather', 'The Dark Knight', 'Pulp Fiction', 'Forrest Gump'],
+    'Genre': ['Drama', 'Crime', 'Action', 'Crime', 'Drama'],
+    'Year': [1994, 1972, 2008, 1994, 1994]
+})
